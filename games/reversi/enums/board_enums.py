79c1cea37398550e8675c1f0f0f94b503581e860
LukasWallisch <wallischlukas@gmail.com>
2019-12-05 23:56:51 +0100

    Added Direction Enum

--- games/reversi/enums/board_enums.py
+++ games/reversi/enums/board_enums.py
@@ -25,3 +25,15 @@ class TileState(Enum):
 
     def __repr__(self):
         return self.name[0]+self.name[-1]
+
+
+class Directions(Enum):
+    """docstring for Directions."""
+    NORTH_WEST = (-1, 1)
+    NORTH = (0, 1)
+    NORTH_EAST = (1, 1)
+    EAST = (1, 0)
+    SOUTH_EAST = (1, -1)
+    SOUTH = (0, -1)
+    SOUTH_WEST = (-1, -1)
+    WEST = (-1, 0)
