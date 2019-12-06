a83462c29eeb0838a9fe1a68641431894154bcb7
LukasWallisch <wallischlukas@gmail.com>
2019-12-06 02:34:58 +0100

    Corrected wrong directions

--- games/reversi/enums/board_enums.py
+++ games/reversi/enums/board_enums.py
@@ -29,11 +29,11 @@ class TileState(Enum):
 
 class Directions(Enum):
     """docstring for Directions."""
-    NORTH_WEST = (-1, 1)
-    NORTH = (0, 1)
-    NORTH_EAST = (1, 1)
+    NORTH_WEST = (-1, -1)
+    NORTH = (0, -1)
+    NORTH_EAST = (1, -1)
     EAST = (1, 0)
-    SOUTH_EAST = (1, -1)
-    SOUTH = (0, -1)
-    SOUTH_WEST = (-1, -1)
+    SOUTH_EAST = (1, 1)
+    SOUTH = (0, 1)
+    SOUTH_WEST = (-1, 1)
     WEST = (-1, 0)
