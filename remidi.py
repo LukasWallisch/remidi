731399455c88b7e36814d7009f792dbb8eac22cf
LukasWallisch <wallischlukas@gmail.com>
2019-12-13 11:32:20 +0100

    remidi main update

--- remidi.py
+++ remidi.py
@@ -3,26 +3,41 @@
 import mylogging
 import time
 from remidi_view import lauchpad
-from remidi_view.layout import ViewGrid, ViewHBar, ViewVBar
+# from remidi_view.layout import ViewGrid, ViewHBar, ViewVBar
+from games.reversi.reversi import Reversi
+from games.reversi.enums import PlayerType
+
+# from games.reversi.board import ReversiBoard
 
 logger = mylogging.setup_logger("remidi")
 
 
 def main():
-    try:
-        logger.info("Hello to remidi")
-        # lp = lauchpad.LaunchpadMiniMk2()
-        g = ViewGrid()
-        vb = ViewVBar()
-        hb = ViewHBar()
-        logger.info(g)
-        logger.info(vb)
-        logger.info(hb)
-        while True:
-            time.sleep(0.01)
-    except KeyboardInterrupt:
-        # lp.reset_all()
-        logger.info("Good Bye")
+    mylogging.set_logger_lvl("launchpad", mylogging.logging.INFO)
+    rv = Reversi(PlayerType.PLAYER_1, 0)
+    t0 = time.time()
+    rv.run()
+    t1 = time.time()
+    logger.debug(t1-t0)
+    # lp= lauchpad.LaunchpadMiniMk2()
+
+    # board2.check_possible(Directions.SOUTH_EAST, 2, 2, PlayerType.PLAYER_1,True)
+    # logger.debug("board2.current_scores\n" + str(board2.current_scores))
+
+    # try:
+    #     logger.info("Hello to remidi")
+    #     # lp = lauchpad.LaunchpadMiniMk2()
+    #     g = ViewGrid()
+    #     vb = ViewVBar()
+    #     hb = ViewHBar()
+    #     logger.info(g)
+    #     logger.info(vb)
+    #     logger.info(hb)
+    #     while True:
+    #         time.sleep(0.01)
+    # except KeyboardInterrupt:
+    #     # lp.reset_all()
+    #     logger.info("Good Bye")
 
 
 if __name__ == "__main__":
